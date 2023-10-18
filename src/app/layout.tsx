3a923c1cb4cda2fd611928a3885ee2d69cc94684
Denilson Junior <dehjr1990@gmail.com>
2023-10-18 10:27:16 -0300

    [REFACTURE]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,8 +18,8 @@ export default function RootLayout({
     return (
         <html lang="pt-br">
             <body className={inter.className}>
-                {children}
                 <Header />
+                {children}
             </body>
         </html>
     );
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,13 +1,22 @@
 import { MenuIcon, ShoppingCartIcon } from "lucide-react";
 import { Button } from "./button";
 import { Card } from "./card";
+import { Sheet, SheetContent, SheetTrigger } from "./sheet";
 
 const Header = () => {
     return (
         <Card className="flex justify-between p-[1.875rem] items-center">
-            <Button size="icon" variant="outline">
-                <MenuIcon />
-            </Button>
+            <Sheet>
+                <SheetTrigger asChild>
+                    <Button size="icon" variant="outline">
+                        <MenuIcon />
+                    </Button>
+                </SheetTrigger>
+
+                <SheetContent>
+                    <h1>Menu</h1>
+                </SheetContent>
+            </Sheet>
 
             <h1 className="font-semibold text-lg">
                 <span className="text-primary">FSW</span> Store
