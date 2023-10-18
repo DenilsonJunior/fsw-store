e41a587a6e1777b37dcb7eb9104bb4eda3d8e3bc
Denilson Junior <dehjr1990@gmail.com>
2023-10-17 22:31:19 -0300

    feat: add button, card and header componnts

--- prettier.config.js
+++ prettier.config.js
@@ -1,11 +1,3 @@
 module.exports = {
     plugins: ['prettier-plugin-tailwindcss'],
-    trailingComma: 'es5',
-    tabWidth: 4,
-    semi: false,
-    singleQuote: true,
-    printWidth: 120,
-    useTabs: false,
-    bracketSpacing: false,
-    jsxBracketSameLine: false,
 }
