a5a014d4ef8c2defd5e32e1765ee7fef71b297c4
Denilson Junior <dehjr1990@gmail.com>
2023-10-23 23:42:22 -0300

    [FACTURE] - Connection and db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,5 +5,6 @@ import { useSession } from "next-auth/react";
 export default function Home() {
     const { data } = useSession();
 
-    return <div>{data?.user?.name}</div>;
+    return <div></div>;
+    // return <div>{data?.user?.name}</div>;
 }
