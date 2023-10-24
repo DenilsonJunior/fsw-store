097b4c12ab6264b6a331324c1cb1b4893bb70497
Denilson Junior <dehjr1990@gmail.com>
2023-10-23 22:01:26 -0300

    feat: add auth handler to header components

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -4,6 +4,7 @@ import {
     HomeIcon,
     ListOrderedIcon,
     LogInIcon,
+    LogOutIcon,
     MenuIcon,
     PercentIcon,
     ShoppingCartIcon,
@@ -11,11 +12,19 @@ import {
 import { Button } from "./button";
 import { Card } from "./card";
 import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
-import { signIn } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Separator } from "./separator";
 
 const Header = () => {
+    const { status, data } = useSession();
+
     const handleLoginClick = async () => {
         await signIn();
+    };
+
+    const handleLogoutClick = async () => {
+        await signOut();
     };
 
     return (
@@ -32,15 +41,52 @@ const Header = () => {
                         Menu
                     </SheetHeader>
 
-                    <div className="mt-2 flex flex-col gap-3">
-                        <Button
-                            onClick={handleLoginClick}
-                            variant="outline"
-                            className="w-full justify-start gap-2"
-                        >
-                            <LogInIcon size={16} />
-                            Fazer Login
-                        </Button>
+                    {status === "authenticated" && data?.user && (
+                        <div className="flex flex-col">
+                            <div className="flex items-center gap-2 py-4">
+                                <Avatar>
+                                    <AvatarFallback>
+                                        {data.user.name?.[0].toUpperCase()}
+                                    </AvatarFallback>
+                                    {data.user.image && (
+                                        <AvatarImage src={data.user.image!} />
+                                    )}
+                                </Avatar>
+                                <div className="flex flex-col">
+                                    <p className="font-medium">
+                                        {data.user.name}
+                                    </p>
+                                    <p className="text-sm font-light opacity-40">
+                                        Boas compras!
+                                    </p>
+                                </div>
+                            </div>
+                            <Separator />
+                        </div>
+                    )}
+
+                    <div className="mt-4 flex flex-col gap-3">
+                        {status === "unauthenticated" && (
+                            <Button
+                                onClick={handleLoginClick}
+                                variant="outline"
+                                className="w-full justify-start gap-2"
+                            >
+                                <LogInIcon size={16} />
+                                Fazer Login
+                            </Button>
+                        )}
+
+                        {status === "authenticated" && (
+                            <Button
+                                onClick={handleLogoutClick}
+                                variant="outline"
+                                className="w-full justify-start gap-2"
+                            >
+                                <LogOutIcon size={16} />
+                                Fazer Logout
+                            </Button>
+                        )}
 
                         <Button
                             variant="outline"
