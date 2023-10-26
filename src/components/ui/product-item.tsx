48d2a74eed99dc01ea2ad085123460a7a46a6e47
Denilson Junior <dehjr1990@gmail.com>
2023-10-25 23:03:41 -0300

    feat: add ProductItem and ProductList components

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -1,14 +1,17 @@
+import { ProductWithTotalPrice } from "@/helpers/product";
 import { Product } from "@prisma/client";
 import Image from "next/image";
+import { Badge } from "./badge";
+import { ArrowDown } from "lucide-react";
 
 interface ProductItemProps {
-    product: Product;
+    product: ProductWithTotalPrice;
 }
 
 const ProductItem = ({ product }: ProductItemProps) => {
     return (
         <div className="flex flex-col gap-4 max-w-[156px]">
-            <div className="bg-accent rounded-lg h-[170px] w-[156px] flex justify-center items-center">
+            <div className="relative bg-accent rounded-lg h-[170px] w-[156px] flex justify-center items-center">
                 <Image
                     src={product.imageUrls[0]}
                     height={0}
@@ -20,9 +23,34 @@ const ProductItem = ({ product }: ProductItemProps) => {
                     }}
                     alt={product.name}
                 />
+
+                {product.discountPercentage > 0 && (
+                    <Badge className="absolute left-3 top-3 px-2 py-[2px]">
+                        <ArrowDown size={14} /> {product.discountPercentage}%
+                    </Badge>
+                )}
             </div>
-            <div>
-                <p className="w-full text-sm overflow-hidden whitespace-nowrap text-ellipsis">{product.name}</p>
+
+            <div className="flex flex-col gap-1">
+                <p className="w-full text-sm overflow-hidden whitespace-nowrap text-ellipsis">
+                    {product.name}
+                </p>
+                <div className="flex items-center gap-2">
+                    {product.discountPercentage > 0 ? (
+                        <>
+                            <p className="font-semibold">
+                                R$ {product.totalPrice.toFixed(2)}
+                            </p>
+                            <p className="opacity-75 line-through text-xs">
+                                R$ {Number(product.basePrice).toFixed(2)}
+                            </p>
+                        </>
+                    ) : (
+                        <p className="font-semibold text-sm">
+                            R$ {product.basePrice.toFixed(2)}
+                        </p>
+                    )}
+                </div>
             </div>
         </div>
     );
