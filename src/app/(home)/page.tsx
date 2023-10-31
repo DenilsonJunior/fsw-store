69438abadb43ab69518db6fd07452ebeecfc4c32
Denilson Junior <dehjr1990@gmail.com>
2023-10-30 21:29:06 -0300

    feat: add second banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -29,8 +29,19 @@ export default async function Home() {
             </div>
 
             <div className="mt-8">
+                <p className="font-bold uppercase pl-5 mb-3">Ofertas</p>
                 <ProductList products={deals} />
             </div>
+
+            <Image
+                src="/banner-home-02.png"
+                width={0}
+                height={0}
+                className="h-auto w-full px-5"
+                sizes="100vw"
+                alt="Até 55% de desconto em mouses!"
+                priority={true}
+            />
         </div>
     );
 }
