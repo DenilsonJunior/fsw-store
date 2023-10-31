852b8c050e665fc1fe474f248bac6ed1ee2f9849
Denilson Junior <dehjr1990@gmail.com>
2023-10-30 23:29:47 -0300

    feat: page category create

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,5 +1,28 @@
-const CatalogPag = () => {
-    return <h1>Catalog</h1>;
+import { Badge } from "@/components/ui/badge";
+import { prismaClient } from "@/lib/prisma";
+import { ShapesIcon } from "lucide-react";
+import CategoryItem from "./components/category-item";
+
+const CatalogPag = async () => {
+    const categories = await prismaClient.category.findMany({});
+
+    return (
+        <div className="flex flex-col gap-8 p-5">
+            <Badge
+                className="w-fit gap-1 text-base uppercase px-3 py-[0.365rem] border-2 border-primary"
+                variant="outline"
+            >
+                <ShapesIcon />
+                Catálogo
+            </Badge>
+
+            <div className="grid grid-cols-2 gap-8">
+                {categories.map((category) => (
+                    <CategoryItem key={category.id} category={category} />
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default CatalogPag;
